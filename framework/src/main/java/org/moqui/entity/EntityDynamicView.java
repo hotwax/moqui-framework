80f520aba9831499d841144cc8b721d93e4e13a5
nutan shinde <nutan.shinde@hotwax.co>
2025-03-27 12:49:41 +0530

    Added the addMemberEntity method in interface as well.

--- framework/src/main/java/org/moqui/entity/EntityDynamicView.java
+++ framework/src/main/java/org/moqui/entity/EntityDynamicView.java
@@ -30,7 +30,7 @@ public interface EntityDynamicView {
     EntityDynamicView addMemberEntity(String entityAlias, String entityName, String joinFromAlias,
                                              Boolean joinOptional, Map<String, String> entityKeyMaps);
     EntityDynamicView addMemberEntity(String entityAlias, String entityName, String joinFromAlias, Boolean joinOptional,
-                                             Map<String, String> entityKeyMaps, List<Map<String, String>> entityConditions, String subSelect);
+                                      Map<String, String> entityKeyMaps, Map<String, String> entityConditions, String subSelect);
 
     EntityDynamicView addRelationshipMember(String entityAlias, String joinFromAlias, String relationshipName,
                                                    Boolean joinOptional);
