ad93d8d81c639b6b317d5bd5aa330fb4f80f7d2e
Karthik Ks <karthik.ks@azentio.com>
2026-02-04 06:36:02 +0000

    Fix RestClient idleTimeout ignoring timeoutSeconds > 30

--- framework/src/main/java/org/moqui/util/RestClient.java
+++ framework/src/main/java/org/moqui/util/RestClient.java
@@ -334,7 +334,7 @@ public class RestClient {
         try {
             Request request = makeRequest(tempFactory != null ? tempFactory : (overrideRequestFactory != null ? overrideRequestFactory : getDefaultRequestFactory()));
             if (timeoutSeconds < 2) timeoutSeconds = 2;
-            request.idleTimeout(timeoutSeconds-1, TimeUnit.SECONDS);
+            request.idleTimeout(timeoutSeconds - 1, TimeUnit.SECONDS);
             // use a FutureResponseListener so we can set the timeout and max response size (old: response = request.send(); )
             CompletableResponseListener listener = new CompletableResponseListener(request, maxResponseSize);
             try {
